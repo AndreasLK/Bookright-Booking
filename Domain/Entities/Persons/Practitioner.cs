25360bf3a7d38f8259b04b44ed7f43840f752135
AndreasLK <Andreaslkrus@gmail.com>
2026-04-29 22:05:42 +0200

    Added personDetails and constructor to Practitioner

--- Domain/Entities/Persons/Practitioner.cs
+++ Domain/Entities/Persons/Practitioner.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Domain.Entities.Persons;
-using Domain.Enums;
+
+using Domain.Value_Objects;
 using Domain.Value_Objects.Ids;
 
-namespace Domain.Entities.People
+namespace Domain.Entities.Persons
 {
         /// <inheritdoc />
         public class Practitioner : Person
@@ -15,17 +12,50 @@ namespace Domain.Entities.People
                 /// </summary>
                 public PractitionerId Id { get; init; }
 
+                private readonly List<Certificate> _certificates = new List<Certificate>();
                 /// <summary>
                 /// Professional certificate identifiers.
                 /// </summary>
-                public List<Guid> CertificateId = new List<Guid>();
+                public IReadOnlyCollection<Certificate> Certificates => this._certificates.AsReadOnly();
 
                 /// <summary>
                 /// Professional nickname or clinical handle.
                 /// </summary>
                 public string? Alias { get; private set; }
 
+                public Practitioner(
+                        PractitionerId id,
+                        string? alias,
+                        PersonDetails details) : base(details: details)
+                {
+                        this.Id = id;
+                        this.Alias = alias;
+                }
 
+                /// <summary>
+                /// Add a new certificate to the Practitioner
+                /// </summary>
+                public void AddCertificate(Certificate certificate)
+                {
+
+                        if (this._certificates.Exists(c => c.CertificateId == certificate.CertificateId))
+                        {
+                                throw new InvalidOperationException(message: $"Practitioner already has Certificate {certificate.CertificateId}");
+                        }
+                        this._certificates.Add(certificate);
+                }
+
+                /// <summary>
+                /// Remove certificate from the Practitioner
+                /// </summary>
+                public void RemoveCertificate(CertificateId id)
+                {
+                        Certificate? certificate = this._certificates.FirstOrDefault(cert => cert.CertificateId == id);
+                        if (certificate is not null)
+                        {
+                                this._certificates.Remove(certificate);
+                        }
+                }
         }
 
 }
