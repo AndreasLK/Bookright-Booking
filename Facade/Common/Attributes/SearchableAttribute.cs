d5775dc978636c9544bb3d4a7920810f345cd328
AndreasLK <Andreaslkrus@gmail.com>
2026-05-03 12:13:31 +0200

    Added Searchable Attribute so the UI knows which attributes are searchable (like phonenumber email etc.) and which are not (medical notes etc.)

---
+++ Facade/Common/Attributes/SearchableAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Facade.Common.Attributes
+{
+    /// <summary>
+    /// Marks a property as searchable to be picked up by the Facade's filtering logic.
+    /// </summary>
+    [AttributeUsage(validOn: AttributeTargets.Property)]
+    public class SearchableAttribute : Attribute
+    {
+    }
+
+}
