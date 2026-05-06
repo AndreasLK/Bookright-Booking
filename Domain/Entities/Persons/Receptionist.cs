45c5cce3bc063d71cbd61dc2fb601a798b1d16c1
AndreasLK <Andreaslkrus@gmail.com>
2026-05-06 18:27:06 +0200

    Removed constructor as it was giving errors. Receptionist is TODO

--- Domain/Entities/Persons/Receptionist.cs
+++ Domain/Entities/Persons/Receptionist.cs
@@ -3,8 +3,8 @@ using Domain.Value_Objects.Ids;
 namespace Domain.Entities.Persons
 {
         /// <inheritdoc />
-        public class Receptionist : Person
-        {
-                public ReceptionistId Id { get; init; }
-        }
+        //public class Receptionist : Person
+        //{
+        //        public ReceptionistId Id { get; init; }
+        //}
 }
