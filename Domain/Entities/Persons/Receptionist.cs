33342d9470196e2fad3eb89219f3716df163c3a6
AndreasLK <Andreaslkrus@gmail.com>
2026-05-06 18:27:23 +0200

    Added TODO comment

--- Domain/Entities/Persons/Receptionist.cs
+++ Domain/Entities/Persons/Receptionist.cs
@@ -3,6 +3,8 @@ using Domain.Value_Objects.Ids;
 namespace Domain.Entities.Persons
 {
         /// <inheritdoc />
+        //TODO: Receptionist class is currently empty and not used. Consider implementing it or removing it if it's not needed.
+
         //public class Receptionist : Person
         //{
         //        public ReceptionistId Id { get; init; }
