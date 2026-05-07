a6a17f5b90519c9e5574f1c32e2896c6fd4f4744
AndreasLK <Andreaslkrus@gmail.com>
2026-05-07 14:58:28 +0200

    Added some repos

---
+++ Domain/Interfaces/Repositories/IBookingRepository.cs
@@ -0,0 +1,9 @@
+using Domain.Entities;
+using Domain.Specifications;
+
+namespace Domain.Interfaces.Repositories
+{
+        public interface IBookingRepository : IRepository<Booking>
+        {
+        }
+}
