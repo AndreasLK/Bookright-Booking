8fa909d84f97802fb294abeb468ea5bf88dfd4ea
AndreasLK <Andreaslkrus@gmail.com>
2026-05-08 09:58:32 +0200

    Added PaidBookingsByCustomerSpecifiaction to allow for search of all bookings that customer has paid before certain date

--- Domain/Specifications/PaidBookingsByCustomerSpecification.cs
+++ Domain/Specifications/PaidBookingsByCustomerSpecification.cs
@@ -1,19 +1,28 @@
 using Domain.Entities;
 using Domain.Value_Objects.Ids;
+using System.Linq.Expressions;
 
 namespace Domain.Specifications
 {
-        public class PaidBookingsByCustomerSpecification
+        public class PaidBookingsByCustomerSpecification : Specification<Booking>
         {
                 private readonly CustomerId _customerId;
+                private readonly DateTime _sinceDate;
 
-                public PaidBookingsByCustomerSpecification(CustomerId customerId)
+                public PaidBookingsByCustomerSpecification(CustomerId customerId, DateTime sinceDate)
                 {
                         ArgumentNullException.ThrowIfNull(argument: customerId, nameof(customerId));
                         this._customerId = customerId;
+                        this._sinceDate = sinceDate;
                 }
 
-
+                public override Expression<Func<Booking, bool>> ToExpression()
+                {
+                        return booking =>
+                        booking.CustomerId == this._customerId &&
+                        booking.Paid != null &&
+                        booking.Timeslot.StartDateTime >= this._sinceDate;
+                }
 
 
         }
