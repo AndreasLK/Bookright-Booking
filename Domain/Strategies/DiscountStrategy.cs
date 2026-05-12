207ca2503878a5739e4f73956136b47e11020fb8
jesjor <jespertjoergensen@gmail.com>
2026-05-12 12:04:57 +0200

    Updated DiscountStrategy to better match DiscountContext

--- Domain/Strategies/DiscountStrategy.cs
+++ Domain/Strategies/DiscountStrategy.cs
@@ -2,6 +2,8 @@ using Domain.Interfaces;
 using Domain.Value_Objects;
 using System.ComponentModel;
 using System.Security.Cryptography.X509Certificates;
+using Domain.Enums;
+using Domain.Value_Objects.Ids;
 
 namespace Domain.Strategies
 {
@@ -9,17 +11,23 @@ namespace Domain.Strategies
         {
                 protected ICurrencyConverter CurrencyConverter { get; private set; }
                 public string DisplayName { get; private set; }
-                public DiscountStrategy(ICurrencyConverter currencyConverter, string displayName)
+
+                private ManualResetEvent _doneEvent;
+                public DiscountStrategy(ICurrencyConverter currencyConverter, string displayName, ManualResetEvent doneEvent)
                 {
                         ArgumentNullException.ThrowIfNull(argument: currencyConverter, paramName: nameof(currencyConverter));
                         ArgumentNullException.ThrowIfNull(argument: displayName, paramName: nameof(displayName));
+                        ArgumentNullException.ThrowIfNull(argument: doneEvent, nameof(doneEvent));
                         this.CurrencyConverter = currencyConverter;
                         this.DisplayName = displayName;
+                        this._doneEvent = doneEvent;
                 }
-                public Money GetFinalPrice(Money totalPurchase, Money currentPurchasePrice)
+                public Money GetFinalPrice(Money totalPurchase, Money currentPurchasePrice, TreatmentId treatmentId, Month customerBirthMonth, List<DateTime> timesUsedCampaign)
                 {
                         ArgumentNullException.ThrowIfNull(argument: totalPurchase, paramName: nameof(totalPurchase));
                         ArgumentNullException.ThrowIfNull(argument: currentPurchasePrice, paramName: nameof(currentPurchasePrice));
+                        ArgumentNullException.ThrowIfNull(argument: treatmentId, paramName: nameof(treatmentId));
+                        ArgumentNullException.ThrowIfNull(argument: timesUsedCampaign, paramName: nameof(timesUsedCampaign));
 
                         Money[] currencyConversionResult = this.CurrencyConverter.ConvertToSame(
                                 targetCurrency: totalPurchase.Currency,
@@ -28,12 +36,18 @@ namespace Domain.Strategies
                         totalPurchase = currencyConversionResult[0];
                         currentPurchasePrice = currencyConversionResult[1];
 
+                        Money result = this.CalculatePrice(
+                                totalPurchase: totalPurchase,
+                                currentPurchasePrice: currentPurchasePrice,
+                                treatmentId: treatmentId,
+                                customerBirthMonth: customerBirthMonth,
+                                timesUsedCampaign: timesUsedCampaign);
 
-                        return this.CalculatePrice(totalPurchase: totalPurchase, currentPurchasePrice: currentPurchasePrice);
+                        this._doneEvent.Set();
+                        return result;
 
                 }
-                protected abstract Money CalculatePrice(Money totalPurchase, Money currentPurchasePrice);
-
+                protected abstract Money CalculatePrice(Money totalPurchase, Money currentPurchasePrice, TreatmentId treatmentId, Month customerBirthMonth, List<DateTime> timesUsedCampaign);
 
 
 
