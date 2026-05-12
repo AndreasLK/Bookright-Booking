3a5dbaddc651a545c3772ae14df9168d56fa0733
jesjor <jespertjoergensen@gmail.com>
2026-05-12 13:12:41 +0200

    Made parrallel execution of discounts work still a TODO point though, so check it out

--- Use Case/BestDiscount/DiscountService.cs
+++ Use Case/BestDiscount/DiscountService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using Domain.Entities;
 using Domain.Interfaces.Repositories;
 using Domain.Value_Objects;
@@ -25,20 +26,35 @@ namespace Use_Case.BestDiscount
                 {
                         ArgumentNullException.ThrowIfNull(context);
 
-                        ManualResetEvent[] workItemsDone = new ManualResetEvent[context.ActiveCampaigns.Count];
+                        var calculatedPrices = new ConcurrentBag<Money>();
 
-                        for(int i = 0; i < context.ActiveCampaigns.Count ;i++)
+                        Parallel.ForEach(context.ActiveCampaigns, campaign =>
+                                {
+                                        List<DateTime> campaignUsage = new List<DateTime>();
+
+                                        if (context.TimeUsedEligbleCampaigns.TryGetValue(campaign.Id, out List<DateTime>? usage))
+                                        {
+                                                campaignUsage = usage;
+                                        }
+
+                                        Money finalPrice = campaign.Strategy.GetFinalPrice(
+                                                totalPurchase: context.TotalHistoricalSpend,
+                                                currentPurchasePrice: context.BasePrice,
+                                                treatmentId: context.TreatmentId,
+                                                customerBirthMonth: context.CustomerBirthMonth,
+                                                timesUsedCampaign: campaignUsage);
+
+                                        calculatedPrices.Add(finalPrice);
+                                });
+
+                        Money? bestDiscountPrice = calculatedPrices.OrderBy(money => money.Value).FirstOrDefault();
+
+                        if (bestDiscountPrice is null || bestDiscountPrice.Value > context.BasePrice.Value) //TODO: Make sure bestDiscount and basePrice are same currency (they should already be but plz future me check this)
                         {
-                                bool containsKey = context.TimeUsedEligbleCampaigns.ContainsKey(key: context.ActiveCampaigns[i].Id);
-                                if (containsKey) continue;
-
-
-                                List<DateTime> campaignUsage = context.TimeUsedEligbleCampaigns[context.ActiveCampaigns[i].Id];
-
-                                workItemsDone[i] = new ManualResetEvent(initialState: false);
-
-                                
+                                return context.BasePrice;
                         }
+
+                        return bestDiscountPrice;
                 }
         }
 }
