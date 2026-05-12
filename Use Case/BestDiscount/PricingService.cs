aace3e1f8575d0c948c08dfec07d0a58defde3c9
AndreasLK <Andreaslkrus@gmail.com>
2026-05-12 09:02:36 +0200

    Added pricingService to be head orchestrator of both discountService and SurchargeService

---
+++ Use Case/BestDiscount/PricingService.cs
@@ -0,0 +1,30 @@
+using Domain.Value_Objects;
+using Domain.Value_Objects.Ids;
+
+
+namespace Use_Case.BestDiscount
+{
+        public class PricingService
+        {
+                private DiscountContextFactory _discountContextFactory;
+                private DiscountService _discountService;
+
+                public PricingService(DiscountContextFactory discountContextFactory, DiscountService discountService)
+                {
+                        ArgumentNullException.ThrowIfNull(discountContextFactory);
+                        ArgumentNullException.ThrowIfNull(discountService);
+
+                        this._discountContextFactory = discountContextFactory;
+                        this._discountService = discountService;
+                }
+
+                public async Task<Money> GetBestPriceAsync(CustomerId customerId, BookingId bookingId)
+                {
+                        DiscountContext context = await this._discountContextFactory.CreateAsync(customerId, bookingId);
+                        Money discountedPrice = this._discountService.CalculateBestDiscount(context);
+
+
+                        return discountedPrice;
+                }
+        }
+}
