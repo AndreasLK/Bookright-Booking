9f1cfd6ebad4f3452c731597d10d8135cd8a0592
AndreasLK <Andreaslkrus@gmail.com>
2026-05-12 09:01:22 +0200

    TODO

---
+++ Use Case/BestDiscount/BestDiscountCalculator.cs
@@ -0,0 +1,16 @@
+using Domain;
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Interfaces;
+using Domain.Interfaces.Repositories;
+using Domain.Specifications;
+using Domain.Value_Objects;
+using Domain.Value_Objects.Ids;
+
+namespace Use_Case.DiscountCalculator
+{
+        public static class BestDiscountCalculator
+        {
+                //TODO:
+        }
+}
