a47d9716b128a439c1e4ff95a1ea7247cdd73449
AndreasLK <Andreaslkrus@gmail.com>
2026-05-12 08:57:39 +0200

    Added IsEligbible method. still TODO

--- Domain/Entities/Campaign.cs
+++ Domain/Entities/Campaign.cs
@@ -11,24 +11,28 @@ namespace Domain.Entities
 
                 public string Description { get; private set; }
 
-                public DateOnly StartDate { get; private set; }
+                public DateTime StartDate { get; private set; }
 
-                public DateOnly EndDate { get; private set; }
+                public DateTime EndDate { get; private set; }
+
+                public TimeSpan Cooldown { get; private set; }
                 public Strategies.DiscountStrategy Strategy { get; private set; }
 
                 public Campaign(
                         CampaignId id,
                         string name,
                         string description,
-                        DateOnly startDate,
-                        DateOnly endDate,
-                        Strategies.DiscountStrategy strategy)
+                        DateTime startDate,
+                        DateTime endDate,
+                        Strategies.DiscountStrategy strategy,
+                        TimeSpan cooldown)
                 {
 
                         ArgumentNullException.ThrowIfNull(argument: id, paramName: nameof(id));
                         ArgumentNullException.ThrowIfNull(argument: name, paramName: nameof(name));
                         ArgumentNullException.ThrowIfNull(argument: description, paramName: nameof(description));
                         ArgumentNullException.ThrowIfNull(argument: strategy, paramName: nameof(strategy));
+
 
                         if (string.IsNullOrWhiteSpace(name))
                         {
@@ -48,7 +52,12 @@ namespace Domain.Entities
                                         message: "End date cannot be before start date",
                                         paramName: nameof(endDate));
                         }
-
+                        if (cooldown < TimeSpan.Zero)
+                        {
+                                throw new ArgumentException(
+                                        message: "Cooldown cannot be negative",
+                                        paramName: nameof(cooldown));
+                        }
 
                         this.Id = id;
                         this.Name = name;
@@ -56,7 +65,10 @@ namespace Domain.Entities
                         this.StartDate = startDate;
                         this.EndDate = endDate;
                         this.Strategy = strategy;
+                        this.Cooldown = cooldown;
                 }
+
+                public bool IsEligible(CampaignUsage )
 
 
 
