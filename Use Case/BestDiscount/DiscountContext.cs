40144ffe7cff7d547b4c6528e7a64db6e78cbcbc
AndreasLK <Andreaslkrus@gmail.com>
2026-05-13 12:26:29 +0200

    Added explicit constructor

--- Use Case/BestDiscount/DiscountContext.cs
+++ Use Case/BestDiscount/DiscountContext.cs
@@ -5,12 +5,33 @@ using Domain.Value_Objects.Ids;
 
 namespace Use_Case.BestDiscount
 {
-        public record DiscountContext(
-                Money BasePrice,
-                TreatmentId TreatmentId,
-                Money TotalHistoricalSpend,
-                List<Campaign> ActiveCampaigns,
-                Dictionary<CampaignId, List<DateTime>> TimeUsedEligbleCampaigns,
-                Month? CustomerBirthMonth
-                );
+        public record DiscountContext
+        {
+                public Money BasePrice { get; init; }
+                public TreatmentId TreatmentId { get; init; }
+                public Money TotalHistoricalSpend { get; init; }
+                public List<Campaign> ActiveCampaigns { get; init; }
+                public Dictionary<CampaignId, List<DateTime>> TimeUsedEligbleCampaigns { get; init; }
+                public Month? CustomerBirthMonth { get; init; }
+
+                public DiscountContext(
+                Money basePrice,
+                TreatmentId treatmentId,
+                Money totalHistoricalSpend,
+                List<Campaign> activeCampaigns,
+                Dictionary<CampaignId, List<DateTime>> timeUsedEligbleCampaigns,
+                Month? customerBirthMonth
+                )
+                {
+                        ArgumentNullException.ThrowIfNull(argument: activeCampaigns, paramName: nameof(activeCampaigns));
+                        ArgumentNullException.ThrowIfNull(argument: timeUsedEligbleCampaigns, paramName: nameof(timeUsedEligbleCampaigns));
+
+                        this.BasePrice = basePrice;
+                        this.TreatmentId = treatmentId;
+                        this.TotalHistoricalSpend = totalHistoricalSpend;
+                        this.ActiveCampaigns = activeCampaigns;
+                        this.TimeUsedEligbleCampaigns = timeUsedEligbleCampaigns;
+                        this.CustomerBirthMonth = customerBirthMonth;
+                }
+        }
 }
