0388a89f10cd76b4a349678dd0411764ce75fe6a
andre <andre@PRIMARY>
2026-05-19 10:44:18 +0200

    Added BookingSummaryDto

---
+++ Facade/Bookings/BookingSummaryDto.cs
@@ -0,0 +1,36 @@
+using Facade.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade.Bookings
+{
+        public class BookingSummaryDto
+        {
+                public Guid Id { get; set; }
+
+                public Guid CustomerId { get; set; }
+
+                [Searchable]
+                public string TreatmentName { get; set; }
+
+                [Searchable]
+                public string PractitionerName { get; set; }
+
+                [Searchable]
+                public string ClinicName { get; set; }
+
+                [Searchable]
+                public string CustomerName { get; set; }
+
+                public DateTime StartTime { get; set; }
+                public DateTime EndTime { get; set; }
+
+                public decimal? AmountPaid { get; set; }
+
+                // Helpful formatted properties for the Razor view
+                public string BookingDateFormatted => this.StartTime.ToString("d MMM yyyy");
+                public string BookingTimeFormatted => $"{this.StartTime:HH:mm} - {this.EndTime:HH:mm}";
+                public string PaymentStatus => this.AmountPaid.HasValue ? "Betalt" : "Mangler Betaling";
+        }
+}
