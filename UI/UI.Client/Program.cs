a4763ddedf6d3bd9e7bb8301c01b40e82df07ebb
andre <andre@PRIMARY>
2026-05-20 17:11:01 +0200

    Added support for new UI

--- UI/UI.Client/Program.cs
+++ UI/UI.Client/Program.cs
@@ -1,12 +1,27 @@
+using Domain.Interfaces.Repositories;
+using Facade.Bookings;
+using Facade.Customers;
+using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace UI.Client
 {
         internal class Program
         {
-                static async Task Main(string[] args)
+                public static async Task Main(string[] args)
                 {
-                        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+                        var builder = WebAssemblyHostBuilder.CreateDefault(args: args);
+
+                        // Register All In-Memory Repositories as Singletons for the Browser
+                        builder.Services.AddSingleton<ICustomerRepository, InMemoryCustomerRepository>();
+                        builder.Services.AddSingleton<IBookingRepository, InMemoryBookingRepository>();
+                        builder.Services.AddSingleton<IClinicRepository, InMemoryClinicRepository>();
+                        builder.Services.AddSingleton<ITreatmentRepository, InMemoryTreatmentRepository>();
+                        builder.Services.AddSingleton<IPractitionerRepository, InMemoryPractitionerRepository>();
+
+                        // Register Facade Services for the Browser
+                        builder.Services.AddScoped<CustomerService>();
+                        builder.Services.AddScoped<BookingService>();
 
                         await builder.Build().RunAsync();
                 }
--- UI/UI/Program.cs
+++ UI/UI/Program.cs
@@ -21,6 +21,14 @@ namespace UI
 
                         builder.Services.AddScoped<Facade.Customers.CustomerService>();
 
+
+                        builder.Services.AddSingleton<Domain.Interfaces.Repositories.IBookingRepository, Infrastructure.Persistence.InMemoryBookingRepository>();
+                        builder.Services.AddSingleton<Domain.Interfaces.Repositories.IClinicRepository, Infrastructure.Persistence.InMemoryClinicRepository>();
+                        builder.Services.AddSingleton<Domain.Interfaces.Repositories.ITreatmentRepository, Infrastructure.Persistence.InMemoryTreatmentRepository>();
+                        builder.Services.AddSingleton<Domain.Interfaces.Repositories.IPractitionerRepository, Infrastructure.Persistence.InMemoryPractitionerRepository>();
+
+                        builder.Services.AddScoped<Facade.Bookings.BookingService>();
+
                         var app = builder.Build();
 
                         // Configure the HTTP request pipeline.
