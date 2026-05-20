a83eba183645b11a5e1992c7d75b41e1b7d0e155
jesjor <jespertjoergensen@gmail.com>
2026-05-20 19:25:21 +0200

    Created IPractitioner Interface

---
+++ Domain/Interfaces/Repositories/IPractitioner.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities.Persons;
+
+namespace Domain.Interfaces.Repositories
+{
+        public interface IPractitionerRepository : IRepository<Practitioner>
+        {
+        }
+}
